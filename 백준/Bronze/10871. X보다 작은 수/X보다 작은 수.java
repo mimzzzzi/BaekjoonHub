dc4c006287a16a8b9d471baa0eb43dc5f9527923
mimzzzzi <103730389+mimzzzzi@users.noreply.github.com>
2022-05-08 12:12:40 +0900

    [Bronze III] Title: X보다 작은 수, Time: 172 ms, Memory: 15260 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc= new Scanner(System.in);
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		int arrNum = sc.nextInt();
-		int num= sc.nextInt();
-		int[] arr=new int[arrNum];
-		for(int i =0;i<arr.length; i++) {
-			arr[i] += sc.nextInt();
-			if(arr[i] < num) {
-				System.out.print(arr[i]+" ");
-			}
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		//반복문에서 다시 입력받을것이므로 초기화 (공백으로 나눔?)
+		st = new StringTokenizer(br.readLine()," ");
+		
+		for(int i = 0; i <N; i++) {
+			// 수열 A 담기
+			int value = Integer.parseInt(st.nextToken());
+			// X와 비교해 sb에 추가하기
+			if(value<X) sb.append(value).append(' ');
 		}
+		System.out.println(sb);
 	}
 	
 }
