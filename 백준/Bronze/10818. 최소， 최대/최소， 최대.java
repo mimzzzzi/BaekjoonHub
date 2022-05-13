d66fd338915b4d98c03d60966ad26c6ad087dfb9
mimzzzzi <103730389+mimzzzzi@users.noreply.github.com>
2022-05-13 12:53:29 +0900

    [Bronze III] Title: 최소, 최대, Time: 1756 ms, Memory: 318572 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[])  {
+	
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+	
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] += sc.nextInt();
+		}
+		
+		int max = arr[0];
+		for (int j = 0; j < arr.length; j++) {
+			if ( max < arr[j]) max = arr[j];
+		}
+		int min = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if(min > arr[i]) min = arr[i];
+		}
+		System.out.println(min +" "+max);
+	}	
+}
